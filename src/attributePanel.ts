b161c04f4d1d7eaacdd75295fc13f7cdffbc23b4
Asmaa Aljuhani <asmaa.aljuhani@utah.edu>
2017-02-01 10:12:47 -0700

    sampledata

--- src/attributePanel.ts
+++ src/attributePanel.ts
@@ -1,7 +1,7 @@
 import * as events from 'phovea_core/src/event';
 import {AppConstants, ChangeTypes} from './app_constants';
-// import * as d3 from 'd3';
-
+import datasets, {IDataSetSpec} from './data/datasets';
+import {csv,dsv} from 'd3-request';
 import {select, selectAll} from 'd3-selection';
 
 import {Config} from './config';
@@ -26,7 +26,11 @@ class attributePanel {
    */
   init() {
 
+    console.log('Hi attribute panel')
+    console.log(datasets)
+
     this.build();
+    this.loadData();
     this.attachListener();
 
     // return the promise directly as long there is no dynamical data to update
@@ -81,6 +85,36 @@ class attributePanel {
 
 
 
+  }
+
+  /**
+   * load data into attribute panel
+   */
+  private loadData(){
+
+    const loadDataset = (dataset: IDataSetSpec) => {
+    const desc = dataset.desc;
+    const file = dataset.url;
+    //setBusy(true);
+    dsv(desc.separator || ',', 'text/plain')(file, (_data) => {
+      console.log(dataset.name)
+      //lineup = initLineup(dataset.name, desc, _data, lineup);
+      //setBusy(false);
+    });
+  };
+
+  {
+    let base = <HTMLElement>document.querySelector('#menu-content');
+    datasets.forEach((d) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<a href="#${d.id}">${d.name}</a>`;
+       loadDataset(d);
+      li.firstElementChild.addEventListener('click', (event) => {
+        loadDataset(d);
+      });
+      base.appendChild(li);
+    });
+  }
   }
 
   private attachListener() {

20f116db62914cb51aebf395485273efb45beb7f
Alexander <alex@sci.utah.edu>
2017-02-22 10:54:38 -0700

    working on data explorations

--- src/dataExplorations.ts
+++ src/dataExplorations.ts
@@ -19,13 +19,18 @@ export default class dataExplorations {
     console.log("Trying to list data");
 
     // listData() returns a list of all datasets loaded by the server
+    // notice the await keyword - you'll see an explanation below
     let all_datasets = await listData();
+    console.log("All loaded datasets:");
+    console.log(all_datasets);
 
-    // TODO: access a dataset by name
+
 
     // here we pick the first dataset and cast it to ITable
     let table: ITable = <ITable> all_datasets[0];
-    console.log("The Table: " + table);
+    console.log("The Table:");
+    console.log(table);
+    console.log("Table Name: " + table.desc.name);
 
     // Here we retrieve the first vector from the table.
     let vector = table.col(0);
@@ -33,32 +38,42 @@ export default class dataExplorations {
     console.log("IDType:" + vector.idtype);
 
     // whenever you access raw data, the data structures return promises, not the data directly.
-    // what you do is, you call a then function, which takes two callbacks as parameters.
-    // The first one handles the "good" case and passes the data in.
-    // The second one handles the "bad" case and handles the error:
-
-    // FIXME: broken, error 500
-    let first_element = vector.at(0).then(
+    // what you do is, you call a then function which takes a callback as a parameter.
+    // This handles the "good" case and passes the data in.
+    // You should also handle the "bad" case in a catch function:
+      const first_promise = vector.at(0).then(
       function (d) {
+        console.log("The data:")
         console.log(d);
-      },
-      function (error) {
+        return d;
+      })
+      .catch(function (error) {
         console.log("Error: " + error);
       });
 
-    console.log(first_element);
-
     // Here is exactly the same code using the arrow notation, for the second element in the vector.
-    // FIXME: broken, error 500
-    let second_element = vector.at(1).then(
-      (d) => console.log(d),
-      (err) => console.log("Error: " + err));
+    const second_promise = vector.at(1).then((d) => d)
+      .catch((err) => console.log("Error: " + err));
 
-    console.log("Second Element" + second_element);
+    Promise.all([first_promise, second_promise]).then(values => {
+      console.log("First Element: " + values[0]);
+      console.log("Second Element: " + values[1]);
+      // all the return values of the promises
+      console.log(values);
+    }).catch((err) => console.log(err));
+
+
+    // a new feature is the await keyword, which makes all of this a lot easier to read:
+    const thirdElement = await vector.at(2);
+    const fourthElement = await vector.at(3);
+
+    console.log('Third Element: ' + thirdElement);
+    console.log('Fourth Element: ' + fourthElement);
 
     // Here we directly access the first element in the first vector:
     // FIXME: broken, error 500
-    table.at(0, 0).then((d) => console.log("data: " + d));
+    const first_value_of_first_vector = await table.at(0, 0);
+    console.log('Accessing the Table for the first element: ' + first_value_of_first_vector);
 
     // TODO: access a vector by name
 
@@ -75,7 +90,6 @@ export default class dataExplorations {
  * @returns {graphData}
  */
 export function create() {
-  console.log("Eu");
 
   // Here we demonstrate how to parse a local table.
   // To do that, put your data file and your json data description in the top-level data directory.
@@ -83,9 +97,6 @@ export function create() {
   // import * as csvUrl from 'file-loader!../data/number_one_artists.csv';
   // The "file-loader! is a webpack feature to load the file TODO: more background?
 
-
-  // We use the parseRemoteTable function that's part of the phovea_d3 package (also included above).
-  // FIXME Broken
   const loading = new Promise((resolve, reject) => {
     tsv(csvUrl, (error, data) => {
       if (error) {
@@ -108,5 +119,7 @@ export function create() {
         console.log('Data of first Column: ' + vectorData.toString());
       });
     });
+
+
   return new dataExplorations();
 }

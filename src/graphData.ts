ae339af5a4bfe45dafb9f3203fba54d0dc98041f
cnobre <carolinanobre@gmail.com>
2017-02-23 15:53:23 -0700

    branches that end in non affected leaves now have a row of their own

--- src/graphData.ts
+++ src/graphData.ts
@@ -58,12 +58,12 @@ class graphData {
 
       //Define 'affected' state. Can be modified by the user with definePrimary();
 //       d['affected'] = +d["affection"] == 100;
-      d['affected'] = Math.random() > 0.7
+      d['affected'] = Math.random() > 0.95
 
       // this.uniqueID.push(+d['id']);
     });
 
-    this.definePrimary('suicide', 'Y', undefined);
+    // this.definePrimary('suicide', 'Y', undefined);
     this.buildTree();
     // this.computeGenerations();
   }
@@ -109,8 +109,9 @@ class graphData {
         });
 
         //No parents found
-        if (maNode.length == 0 || paNode.length == 0){
-          node['ma']=undefined; node['pa']= undefined;
+        if (maNode.length == 0 || paNode.length == 0) {
+          node['ma'] = undefined;
+          node['pa'] = undefined;
         }
 
         // If found parents, create edges between parent and children, spouses, and add references to build tree
@@ -168,34 +169,41 @@ class graphData {
    */
   public findLastLeaf(node) {
 
+    console.log('find last leaf ', node)
     //Base case -> leaf node w/ no spouse
-    if (node['spouse'].length==0 && !node['hasChildren']){
+    if (node['spouse'].length == 0 && !node['hasChildren']) {
       return node['y'];
     }
 
     //Base case -> leaf node w/ spouse
-    else if (node['spouse'].length>0 && !node['hasChildren']){
+    else if (node['spouse'].length > 0 && !node['hasChildren']) {
       let levels = [node['y']];
-      node['spouse'].forEach((s)=>{levels.push(s['y'])});
+      node['spouse'].forEach((s) => {
+        levels.push(s['y'])
+      });
       return max(levels);
     }
 
     //Has only one spouse -> call find lastLeaf on each of their children.
-    else if (node['spouse'].length==1 ){
-      return min(node['children'].map((child)=>{return this.findLastLeaf(child);}));
+    else if (node['spouse'].length == 1) {
+      return min(node['children'].map((child) => {
+        return this.findLastLeaf(child);
+      }));
     }
 
     //Has more than one spouse, find last Leaf of all children in these relationships
-    else if (node['spouse'].length>1 ){
-      return  min(node['spouse'].map((spouse)=>{
-        return min(spouse['children'].map((child)=>{return this.findLastLeaf(child)}));
+    else if (node['spouse'].length > 1) {
+      return min(node['spouse'].map((spouse) => {
+        return min(spouse['children'].map((child) => {
+          return this.findLastLeaf(child)
+        }));
       }));
     }
   };
 
   /**
    *
-   * This function hides all the nodes that descend from a given starting point. .
+   * This function hides all the nodes that descend from a given starting point. to the end of that branch.
    *
    * @param startIndex - y value (row number) for the starting point.
    */
@@ -204,166 +212,171 @@ class graphData {
     let Y = startIndex;
 
     //Find the non hidden node in that row
-    let startNode = this.nodes.filter((node)=>{
+    let startNode = this.nodes.filter((node) => {
       return (node['y'] == startIndex && !node['hidden']);
     })
 
     //Iterate down that branch to find the last index of this family.
-    let endIndex =  this.findLastLeaf(startNode[0]);
+    let endIndex = this.findLastLeaf(startNode[0]);
 
     this.nodes.sort((a, b) => {
       return b['Y'] - a['Y']
     });
 
     //Assign a row for each affected case within the range to be collapsed;
-    this.nodes.filter((node)=>{return node['y'] <= startIndex && node['y'] >= endIndex}).forEach((node) => {
+    this.nodes.filter((node) => {
+      return node['y'] <= startIndex && node['y'] >= endIndex
+    }).forEach((node) => {
 
-        //non affected leaf nodes
-        if (!node['hasChildren'] && !node['affected']) {
+      //non affected leaf nodes
+      if (!node['hasChildren'] && !node['affected']) {
 
-          let ma = node['ma'];
-          let pa = node['pa'];
+        let ma = node['ma'];
+        let pa = node['pa'];
 
-          console.log(ma,pa);
-
-          //If both parents are affected
-          if (ma['affected'] && pa['affected']) {
-
-            //place kid grid in the middle
-            node['y'] = (ma['y'] + pa['y']) / 2
-          }
-          //If only one or neither parent is affected,
-          else if (ma['affected']) {
-            if (node['sex'] == 'M')
-              node['y'] = ma['y'] - 0.2;
-            else
-              node['y'] = ma['y'] + .2
-          }
-          else if (pa['affected']) {
-            if (node['sex'] == 'M')
-              node['y'] = pa['y'] - 0.2;
-            else
-              node['y'] = pa['y'] + 0.2
-          }
-          else {
-            if (node['sex'] == 'M')
-              node['y'] = pa['y'];
-            else
-              node['y'] = ma['y'];
-          }
-
-          //Starting point for the kid grid
-          if (!node['affected'])
-            node['x'] = ma['x'] + 5;
-        } //end leaf nodes
+        //If both parents are affected
+        if (ma['affected'] && pa['affected']) {
+          //place kid grid in the middle
+          node['y'] = (ma['y'] + pa['y']) / 2
+        }
+        //Only mother is affected,
+        else if (ma['affected']) {
+          if (node['sex'] == 'M')
+            node['y'] = ma['y'] - 0.2;
+          else
+            node['y'] = ma['y'] + .2
+        }
+        //Only father is affected
+        else if (pa['affected']) {
+          if (node['sex'] == 'M')
+            node['y'] = pa['y'] - 0.2;
+          else
+            node['y'] = pa['y'] + 0.2
+        }
+        //Neither parent is affected
         else {
-          if (!node['affected']) {
+          if (node['sex'] == 'M')
+            node['y'] = pa['y'];
+          else
+            node['y'] = ma['y'];
+        }
 
-            let spouse = node['spouse'];
+        //Starting point for the kid grid
+        node['x'] = ma['x'] + 6;
+      } //end leaf nodes
 
-            if (spouse.length > 0) { //they had at least one partner
-              spouse = spouse[0];
+      //Affected nodes and non-leaf nodes
+      else {
+        //Non-leaf and non-affected nodes
+        if (!node['affected']) {
 
-              if (spouse['affected']) {
-                if (node['sex'] == 'M')
-                  node['y'] = spouse['y'] - 0.2;
-                else
-                  node['y'] = spouse['y'] + 0.2;
-              }
-// 		          	node['y']= spouse['y']
-              else {
-                if (node['sex'] == 'M')
-                  node['y'] = Y - 0.2;
-                else
-                  node['y'] = Y + 0.2;
-              }
-            }
-          }
-          else { //affected node
-            node['y'] = Y;
-            //Search for spouse and place in the right location (if not affected)
-            let edge = this.parentParentEdges.filter((d) => {
-              return node['sex'] == 'M' ? d['pa'] == node : d['ma'] == node
-            });
+          let spouse = node['spouse'];
 
-            if (edge.length > 0) {
-              let spouse;
+          if (spouse.length > 0) { //they had at least one partner
+            spouse = spouse[0];
+
+            //Affected Spouse
+            if (spouse['affected']) {
               if (node['sex'] == 'M')
-                spouse = edge[0]['ma'];
+                node['y'] = spouse['y'] - 0.2;
               else
-                spouse = edge[0]['pa'];
-
-              if (!spouse['affected']) {
-                if (node['sex'] == 'M')
-                  spouse['y'] = Y //-0.2;
-                else
-                  spouse['y'] = Y //+0.2;
-              }
-// 		          	spouse['y']= Y //-1 +0.4;
-
+                node['y'] = spouse['y'] + 0.2;
             }
+            //Non affected Spouse
+            else {
+              if (node['sex'] == 'M')
+                node['y'] = Y - 0.2;
+              else
+                node['y'] = Y + 0.2;
+            }
+
           }
-
-          //Place Mom and Dad Nodes on top of Each other (at the dad's x location) .
-          //Iterate through parent-child edges and find the spouse
-          this.parentParentEdges.forEach((edge) => {
-            if (node == edge['pa'] || node == edge['ma']) {
-              node['x'] = edge['pa']['x']
-            }
-          })
 
         }
 
-        if (node['affected'])
-          Y = Y - 1;
-
+        //Affected Nodes
         else {
-
-          let edge = this.parentParentEdges.filter((d) => {
-            return node['sex'] == 'M' ? d['pa'] == node : d['ma'] == node
-          });
-          let spouse;
-
-          if (edge.length > 0) {
-            if (node['sex'] == 'M')
-              spouse = edge[0]['ma'];
-            else
-              spouse = edge[0]['pa'];
-          }
-
-
-          let children = this.parentChildEdges.filter((d) => {
-            if (node['sex'] == 'M')
-              return (d.ma == spouse && d.pa == node);
-            else
-              return (d.pa == spouse && d.ma == node);
-          });
-
-
-          let newBranch = true;
-          children.forEach((c) => {
-            newBranch = newBranch && !c['target']['hasChildren'] && !c['target']['affected']
-          })
-
-          if (newBranch) {
-            console.log('new branch')
-// 	          Y = Y - 1;
-          }
-
-
-          node['hidden'] = true;
+          node['y'] = Y;
+//             //Search for spouse and place in the right location (if not affected)
+//             let edge = this.parentParentEdges.filter((d) => {
+//               return node['sex'] == 'M' ? d['pa'] == node : d['ma'] == node
+//             });
+//
+//             if (edge.length > 0) {
+//               let spouse;
+//               if (node['sex'] == 'M')
+//                 spouse = edge[0]['ma'];
+//               else
+//                 spouse = edge[0]['pa'];
+//
+//               if (!spouse['affected']) {
+//                 if (node['sex'] == 'M')
+//                   spouse['y'] = Y //-0.2;
+//                 else
+//                   spouse['y'] = Y //+0.2;
+//               }
+// // 		          	spouse['y']= Y //-1 +0.4;
+//
+//             }
         }
+
+        //Place Mom and Dad Nodes on top of Each other (at the dad's x location)
+        if (node['sex'] == 'F' && node['spouse'].length > 0) {
+          node['x'] = node['spouse'][0]['x']; //need to figure out how to handle multi spouse situations 2/23/2017
+        }
+
+      }
+
+      if (node['affected'])
+        Y = Y - 1;
+      else {
+
+        //Check if you are at the end of a branch w/ only unaffected leaf children.
+
+        let unaffectedLeafChildren = !this.hasAffectedChildren(node);
+
+        //If current node has only unaffected leaf children and does not have any affected spouses and is not a leaf
+        let newBranch = unaffectedLeafChildren && node['hasChildren'] &&
+          node['spouse'].reduce((acc, spouse) => {return acc && !spouse['affected']}, true)
+          && node['Y'] < max(node['spouse'].map((s) => {return s['Y']}));
+
+
+        if (newBranch) {
+          Y = Y - 1;
+          console.log('new Branch for node , ',  node['y'] )
+        }
+
+        node['hidden'] = true;
+      }
     });
 
 
     //Get rid of blank rows;
-    this.nodes.filter((d)=>{return d.y >= endIndex}).forEach((node) => {
-      node['y'] = node['y'] - (Y - endIndex+1);
+    this.nodes.filter((d) => {
+      return d.y >= endIndex
+    }).forEach((node) => {
+      node['y'] = node['y'] - (Y - endIndex + 1);
     })
   }
 
-}
 
+  /**
+   *
+   * This function returns true if this node has any affected leaf children.
+   *
+   * @param node to query
+   * @return true/false indicating whether this node has any affected leaf children
+   */
+  private hasAffectedChildren(node){
+    let value = node['children'].reduce((acc, child) => {
+      return acc && !child['affected'] && !child['hasChildren']
+    }, true)
+
+    return !value;
+
+  }
+
+}
 
 
 /**

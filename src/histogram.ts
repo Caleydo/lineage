eb140da8be86ef4d1bdf6c3fdcee134f6cf378a4
Asmaa Aljuhani <asmaa.aljuhani@utah.edu>
2017-03-06 20:16:00 -0700

    an event is fired when a user pick a category as a value for primary/secondary attributes

--- src/histogram.ts
+++ src/histogram.ts
@@ -5,11 +5,13 @@ import {axisTop,axisLeft,axisRight,axisBottom} from 'd3-axis';
 import {set} from 'd3-collection';
 import {ICategoricalVector, INumericalVector} from 'phovea_core/src/vector/IVector';
 import {IAnyVector} from 'phovea_core/src/vector';
+import * as events from 'phovea_core/src/event';
 
 
 class Histogram {
   //DOM elements
   private $node;
+  private attrName;
   private $histogram;
   private $histogramCols;
   //settings
@@ -37,7 +39,8 @@ class Histogram {
    * initalize the histogram and return a promise
    * that is resolved as soon the view is completely initialized
    */
-  async init(dataVec) {
+  async init(name, dataVec) {
+    this.attrName = name;
     this.dataVec = dataVec;
     this.categories = dataVec.desc.value.categories;
     this.data = await this.dataVec.data();
@@ -55,27 +58,27 @@ class Histogram {
    */
   private prepData() {
     const categories = set(this.data).values();// return array of unique values
-    for(const cat of categories){
-      const val = this.data.filter(function(elm){
+    for (const cat of categories) {
+      const val = this.data.filter(function (elm) {
         return elm === cat;
       });
-      this.histData.push({key: cat, value:val.length});
+      this.histData.push({key: cat, value: val.length});
     }
-/*
-    for (const d of this.data) {
-      if (this.histData.length === 0) {
-        this.histData.push({name: d, value: 1});
-      } else {
-        for (const c of this.histData) {
-          if (c.name === d) {
-            c.value = c.value + 1;
-          } else {
-            this.histData.push({name: d, value: 1});
-          }
-        }
-      }
-    }
-*/
+    /*
+     for (const d of this.data) {
+     if (this.histData.length === 0) {
+     this.histData.push({name: d, value: 1});
+     } else {
+     for (const c of this.histData) {
+     if (c.name === d) {
+     c.value = c.value + 1;
+     } else {
+     this.histData.push({name: d, value: 1});
+     }
+     }
+     }
+     }
+     */
 
   }
 
@@ -84,44 +87,64 @@ class Histogram {
    */
   private async build() {
     const padding = 20;
-     this.margin = {top: 20, right: 20, bottom: 30, left: 40};
-     this.width = 300 - this.margin.left - this.margin.right - padding ;
-     this.height = 200 - this.margin.top - this.margin.bottom - padding;
-     //scales
-     this.xScale.rangeRound([0, this.width]).padding(0.6)
-     .domain(this.histData.map(function(d) { return d.key; }));
-     this.yScale.rangeRound([this.height, 0])
-       .domain([0, this.data.length]);
-       //.domain([0, max(this.histData, function(d){return d.value;})]);
-     const g = this.$node.append('g')
-    .attr('transform', 'scale(0.8,0.8) translate(20,20)');
+    this.margin = {top: 20, right: 20, bottom: 30, left: 40};
+    this.width = 300 - this.margin.left - this.margin.right - padding;
+    this.height = 200 - this.margin.top - this.margin.bottom - padding;
+    //scales
+    this.xScale.rangeRound([0, this.width]).padding(0.6)
+      .domain(this.histData.map(function (d) {
+        return d.key;
+      }));
+    this.yScale.rangeRound([this.height, 0])
+      //.domain([0, this.data.length]);
+      .domain([0, max(this.histData, function (d) {
+        return d.value;
+      })]);
+    const g = this.$node.append('g')
+      .attr('transform', 'scale(0.8,0.8) translate(20,20)');
     //axis
     const xAxis = g.append('g')
       .attr('class', 'axis axis--x')
-      .attr('transform', 'translate('+ padding +',' + this.height + ')')
+      .attr('transform', 'translate(' + padding + ',' + this.height + ')')
       .call(axisBottom(this.xScale));
     const yAxis = g.append('g')
-    .attr('class', 'axis axis--y')
-      .attr('transform', 'translate('+ padding +',0)')
-      .call(axisLeft(this.yScale.nice()).ticks(3));
-    /*.append('text')
+      .attr('class', 'axis axis--y')
+      .attr('transform', 'translate(' + padding + ',0)')
+      .call(axisLeft(this.yScale.nice()).ticks(3))
+      .append('text')
       .attr('transform', 'rotate(-90)')
       .attr('y', 6)
       .attr('dy', '0.71em')
       .attr('text-anchor', 'end')
-      .text('Value');*/
+      .text('Value');
 
-     g.append('g')
-       .attr('transform', 'translate('+ padding +',0)')
-       .selectAll('.bar')
-    .data(this.histData)
-    .enter().append('rect')
+    g.append('g')
+      .attr('transform', 'translate(' + padding + ',0)')
+      .selectAll('.bar')
+      .data(this.histData)
+      .enter().append('rect')
       .attr('class', 'bar')
-      .attr('x', (d)=> { return this.xScale(d.key); })
-      .attr('y', (d)=> { return this.yScale(d.value); })
+      .attr('x', (d)=> {
+        return this.xScale(d.key);
+      })
+      .attr('y', (d)=> {
+        return this.yScale(d.value);
+      })
       .attr('width', this.xScale.bandwidth())
-      .attr('height', (d)=> { return this.height - this.yScale(d.value); })
-       .attr('fill', 'rgb(226, 225, 224)');
+      .attr('height', (d)=> {
+        return this.height - this.yScale(d.value);
+      })
+      .attr('fill', 'rgb(226, 225, 224)')
+      .attr('attribute', this.attrName );
+
+    selectAll('.bar').on('click', function (d) {
+      select(this).classed('picked', true);
+      const item = {
+          name: select(this).attr('attribute'),
+          value: d.key
+        };
+        events.fire('attribute_cat_picked', item);
+    });
 
 
   }

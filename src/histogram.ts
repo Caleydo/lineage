7ad27214864548ecb6c2278f04a68d8e8f677730
cnobre <carolinanobre@gmail.com>
2017-03-13 16:59:26 -0600

    remove build() from histogram

--- src/histogram.ts
+++ src/histogram.ts
@@ -50,7 +50,7 @@ class Histogram {
     this.categories = dataVec.desc.value.categories;
     this.data = await this.dataVec.data();
 
-    this.build();
+    // this.build();
     this.update(dataVec);
     this.attachListener();
 

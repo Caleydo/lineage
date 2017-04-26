8fe405aff9a268ba4467397bbcf9c039bdf842d8
Alexander <alex@sci.utah.edu>
2017-04-26 12:29:40 -0600

    updated text

--- src/index.ts
+++ src/index.ts
@@ -30,10 +30,10 @@ dialogContent.innerHTML = `
     <h2 style="font-size: 1.3em">Lineage: Visualizing Multivariate Clinical Data in Genealogy Graphs</h2>
              <p>
                 Lineage is developed by Carolina Nobre and Alexander Lex at the <a href="http://vdl.sci.utah.edu">Visualization
-                Design Lab</a>, University of Utah, with contributions form <a href="http://gehlenborglab.org">Nils
+                Design Lab</a>, University of Utah, in collaboration with <a href="http://gehlenborglab.org">Nils
                 Gehlenborg</a>, Harvard Medical School, and Hilary Coon, Department of Psychiatry, University of Utah.
             </p>
-            <p><b>Details on the associated publication and acknowledgements of other contributors are availabe on the <a href="http://vdl.sci.utah.edu/publications/2017_preprint_lineage/">VDL website</a>.</b></p>
+            <p><b>Details on the associated publication and acknowledgements of other contributors and funding sources are availabe on the <a href="http://vdl.sci.utah.edu/publications/2017_preprint_lineage/">VDL website</a>.</b></p>
             <p>A video that provides an overview of Lineage is available on <a href="https://www.youtube.com/watch?v=35oa5IvgtS0">YouTube</a>.</p>   
             <p>The dataset provided is an anonymized subset of ten families from a suicide study based on the <a href="https://healthcare.utah.edu/huntsmancancerinstitute/research/updb/">Utah Population Database.</a></p>
 </div>

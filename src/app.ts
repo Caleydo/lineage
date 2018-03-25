44320a82a25d7a98993ff42adb79badb7b87081f
cnobre <carolinanobre@gmail.com>
2018-03-24 21:16:57 -0600

    moved node type attribute menu and filtering to top bar. Also made room to fit all labels.

--- src/app.ts
+++ src/app.ts
@@ -37,8 +37,8 @@ export class App {
 
     // this.$node.select('#col1').append('div').attr('id', 'data_selection');
     this.$node.select('#col1').append('div').attr('id', 'pathViewerAccordion').attr('class','panel-group');
-    this.$node.select('#col1').append('div').attr('id', 'queryInput').attr('class','panel panel-default');
-    this.$node.select('#col1').append('div').attr('id', 'subGraphInput').attr('class','panel panel-default');
+    // this.$node.select('#col1').append('div').attr('id', 'queryInput').attr('class','panel panel-default');
+    // this.$node.select('#col1').append('div').attr('id', 'subGraphInput').attr('class','panel panel-default');
     this.$node.select('#col1').append('div').attr('id', 'searchBar').attr('class','panel panel-default');
     this.$node.select('#col1').append('div').attr('id', 'toolBar').attr('class','panel panel-default');
       this.$node.select('#col1').append('div').attr('id', 'accordion').attr('class','panel-group');
